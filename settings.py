f5c0ffff69dd494ca2cf4e1b63e8d1241a619d26
schirrock <so.to.the@gmail.com>
2025-05-18 18:25:15 +0330

    Update port mappings in docker-compose and modify MongoDB URI environment variable
    1. compose.yml:
       - Swapped port mappings for web and MongoDB services to align with standard practices.
    
    2. settings.py:
       - Changed environment variable from 'MONGO_URI' to 'MONGODB_URI' for better clarity and consistency.

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ from os import getenv
 
 #databases
 LOCAL_MONGO_URI = 'mongodb://127.0.0.1:27017/takhfif_class?authSource=admin'
-MONGO_URI = getenv('MONGO_URI', LOCAL_MONGO_URI)
+MONGO_URI = getenv('MONGODB_URI', default=LOCAL_MONGO_URI)
 MONGODB_SETTINGS = dict(host=MONGO_URI)
