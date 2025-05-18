ed40c2c6c1b30440608e8a4736502aff8a127142
schirrock <so.to.the@gmail.com>
2025-05-18 18:04:16 +0330

    Update MongoDB configuration in docker-compose and settings files
    1. compose.yml:
       - Updated MongoDB URI to include authentication credentials.
       - Added environment variables for MongoDB root username and password.
       - Set Docker
    
    2. settings.py:
       - Introduced a local MongoDB URI and updated MONGO_URI to use environment variables for better flexibility.

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
-
-
+from os import getenv
 
 #databases
-MONGO_URI = 'mongodb://127.0.0.1:27017/takhfif_class?authSource=admin' 
+LOCAL_MONGO_URI = 'mongodb://127.0.0.1:27017/takhfif_class?authSource=admin'
+MONGO_URI = getenv('MONGO_URI', LOCAL_MONGO_URI)
 MONGODB_SETTINGS = dict(host=MONGO_URI)
