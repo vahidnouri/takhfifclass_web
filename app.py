d5735f2480c8a1dd871cf70efcd97385cca3ffda
vahidnouri <nouri_vahid@hotmail.com>
2025-05-21 12:32:42 +0330

    Update to use in docker

--- app.py
+++ app.py
@@ -203,5 +203,5 @@ def full_results(query):
     }
     return render_template("search_results.html", courses=results, query=query, data=data)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
