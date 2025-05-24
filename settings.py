6d41220552e502cd0acead9e47c01d40ac6556a6
vahidnouri <nouri_vahid@hotmail.com>
2025-05-24 14:08:10 +0330

    Use this name instead of takhfif_class
    Update the database name.

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 from os import getenv
 
 #databases
-LOCAL_MONGO_URI = 'mongodb://127.0.0.1:27017/takhfif_class?authSource=admin'
+LOCAL_MONGO_URI = 'mongodb://127.0.0.1:27017/scraped_data?authSource=admin'
 MONGO_URI = getenv('MONGODB_URI', default=LOCAL_MONGO_URI)
 MONGODB_SETTINGS = dict(host=MONGO_URI)
